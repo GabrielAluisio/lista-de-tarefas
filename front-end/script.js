e2ac2bdf0d091bebcde284ddc7c08d46f55c4c89
GabrielDiniz <aluisiogabrieldiniz@gmail.com>
2025-07-01 22:35:41 -0300

    Função adicionarTarefa implementada com criação dinâmica dos elementos

---
+++ front-end/script.js
@@ -0,0 +1,54 @@
+function adicionarTarefa(){
+    const campoInput = document.querySelector('input.campo')
+    const tarefas = document.getElementById('tarefas')
+
+    // Pega o texto digitado e tira os espaços no começo e no final.
+    const texto = campoInput.value.trim()
+
+    // confere se o usúario digitou nada. 
+    if (texto == ''){
+        alert('Digite alguma tarefa antes de adicionar!')
+        return
+    }
+
+    // criando uma div com uma class:".item-tarefa".
+    const novaTarefa = document.createElement('div')
+    novaTarefa.classList.add('item-tarefa')
+    
+    // Criando o checkbox
+    const checkbox = document.createElement('input')
+    checkbox.classList.add('checkbox')
+
+    // Crianso o label. Exe: <label class="tarefa"> Texto </label>
+    const label = document.createElement('label')
+    label.classList.add('tarefa')
+    label.textContent = texto; // Adicionar o texto dentro do label
+
+    // botão Editar
+    const editar = document.createElement('button')
+    editar.classList.add('editar')
+    editar.setAttribute('aria-label', 'Editar tarefa')
+    // icone
+    editar.innerHTML = '<span class="material-symbols-outlined">edit</span>'
+
+    // botão Delete
+    const excluir = document.createElement('button')
+    excluir.classList.add('excluir')
+    excluir.setAttribute('aria-label', 'Excluir tarefa')
+    // icone
+    excluir.innerHTML = '<span class="material-symbols-outlined">delete</span>'
+
+    // Adicionando os elementos na div novaTarefa: (div class="item-tarefa")
+    novaTarefa.appendChild(checkbox)
+    novaTarefa.appendChild(label)
+    novaTarefa.appendChild(editar)
+    novaTarefa.appendChild(excluir)
+
+    // Adicionando a novaTarefa no div id='tarefas'
+    tarefas.appendChild(novaTarefa)
+
+    campoInput.value = ''
+
+
+
+}
